3b5c76a9c93029997ad818dca4e8396cf7ffacd1
Yehor <yehor.shapoval2000@gmail.com>
2025-03-10 19:09:53 +0100

    created userDTO for Get and Post

--- DTOModels/UserDTO.cs
+++ DTOModels/UserDTO.cs
@@ -1,6 +1,17 @@
 namespace BrainsToDo.DTOModels;
-
-public class UserDTO
 {
-    
+    public class PostUserDTO
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public class GetUserDTO
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
 }
