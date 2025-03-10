cdf0bbb87e2883ab05e6d3dbb4ce4995dd901eeb
Yehor <yehor.shapoval2000@gmail.com>
2025-03-10 19:12:31 +0100

    changed GetUserDTO

--- DTOModels/UserDTO.cs
+++ DTOModels/UserDTO.cs
@@ -1,17 +1,9 @@
 namespace BrainsToDo.DTOModels;
-{
+
     public class PostUserDTO
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
     }
 
-    public class GetUserDTO
-    {
-        public int id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
-    }
-}
+    
