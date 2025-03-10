e50ae20d069bab0307b7bc30d48e430383cb171b
Yehor <yehor.shapoval2000@gmail.com>
2025-03-10 20:04:42 +0100

    created empty  CRUD for UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,41 @@
-﻿namespace BrainsToDo.Controllers;
+﻿using BrainsToDo.DTOModels;
+using BrainsToDo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-public class UserController
+namespace BrainsToDo.Models;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
 {
-    
+    public UserController()
+    {
+    }
+
+    [HttpGet]
+    public IActionResult Get(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IStatusCodeActionResult Creat(User user)
+    {
+        return Ok();
+    }
+
+    [HttpPut(template: "{id}")]
+    public IStatusCodeActionResult Update(int id, User user)
+    {
+        return Ok();
+    }
+
+    [HttpDelete(template: "{id}")]
+    public IStatusCodeActionResult Delete(int id)
+    {
+        return Ok();
+    }
+
 }
