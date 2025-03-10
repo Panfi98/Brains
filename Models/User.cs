fe70e6d3162e84571967d78e50938cf0ffa25c04
Yehor <yehor.shapoval2000@gmail.com>
2025-03-10 19:05:33 +0100

    created model user

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BrainsToDo.Models;
 
-public class User
-{
-    
-}
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("firstName")]
+        public string firstName { get; set; }
+        [Column("lastName")]
+        public string lastName { get; set; }
+        [Column("createdAt")]
+        public DateTime createdAt { get; set; }
+        [Column("updatedAt")]
+        public DateTime updatedAt { get; set; } 
+    }
