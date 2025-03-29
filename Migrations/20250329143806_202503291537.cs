003118bc2bbde03d471dfe7271ba0d4541bc8bd3
Yehor <yehor.shapoval2000@gmail.com>
2025-03-29 15:39:41 +0100

    made database migrations 202503291537

---
+++ Migrations/20250329143806_202503291537.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrainsToDo.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202503291537 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_User_userId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_userId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Tasks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_userId",
+                table: "Tasks",
+                column: "userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_User_userId",
+                table: "Tasks",
+                column: "userId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
