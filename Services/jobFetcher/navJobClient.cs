7ca980d119cb6efd6c66cfd53642ced140a3a4b8
panfi98 <danielpanf@gmail.com>
2025-05-18 23:08:32 +0200

    introduced navJobClient logic

--- Services/jobFetcher/navJobClient.cs
+++ Services/jobFetcher/navJobClient.cs
@@ -2,5 +2,26 @@ namespace BrainsToDo.Services.jobFetcher;
 
 public class navJobClient
 {
-    
+    public HttpClient _HttpClient;
+
+    public navJobClient(HttpClient httpClient)
+    {
+        _HttpClient = httpClient;
+    }
+
+    public async Task<T?> GetAsync<T>(string url)
+    {
+        try
+        {
+            var response = await _HttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+        
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Request error: {ex.Message}");
+            return default;
+        }
+    }
 }
